daf802b5ec639c5ea92313d394773dba28cc40e5
Abhishekkar123 <abhi.kar.2500@gmail.com>
2025-12-25 15:04:17 +0530

    change the file

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose')
 const {loadPlanetData}=require('./models/planet.model');
 const server=http.createServer(app);
 
-const MONGO_URL="mongodb+srv://chinukar2001_db_user:nasaProject@cluster0.wbw1qn3.mongodb.net/?appName=Cluster0"
+
 const PORT=process.env.PORT || 8000;
 
 
